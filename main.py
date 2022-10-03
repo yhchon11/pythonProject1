6fa881149661a85d7238c43895e3008a279ea9cb
yhchon11 <102776511+yhchon11@users.noreply.github.com>
2022-10-03 17:40:45 +0900

    Updates 2 on GitHub Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Updates on GitHub")
 
 #Updates in local master branch
 print("Hello World Updates on Local Master Branch ")
+
+#Updates on mainbranch of GitHub
+print("Hello World Updates 2 on GitHub Only")
